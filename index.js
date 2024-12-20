8cf68a4b39c21b5b15a7c00dbac57499203d50e4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-20 18:18:04 +0600

    Goodbye function

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // callback = a function that is passed as an argument to another function.
 
 hello();
+goodBye()
 
 function hello() {
   console.log("hello");
+}
+
+function goodBye(){
+  console.log("Goodbye!");
 }
