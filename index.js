5cb262dc1c8829de7e78747b7178d03248a87d03
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-20 18:32:34 +0600

    displayPage function callback

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // callback = a function that is passed as an argument to another function.
-sum(displayConsole, 1, 2);
+sum(displayPage, 1, 2);
 
 function sum(callback, x, y){
   let result = x+y;
@@ -8,4 +8,8 @@ function sum(callback, x, y){
 
 function displayConsole(result){
   console.log(result);
+}
+
+function displayPage(result) {
+  document.getElementById('myH1').textContent = result;
 }
